7ff969cf19c51b4684080e9ec71bc7464dc86fcc
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2021-01-30 21:18:07 +0100

    Initial commit

---
+++ src/main/java/com/odent/odentcore/OdentCoreApplication.java
@@ -0,0 +1,13 @@
+package com.odent.odentcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdentCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdentCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odent/odentcore/OdentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odent.odentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdentCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
